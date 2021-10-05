da99bd1d88f8c946786537a96a48883fa1335789
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-04 20:40:21 -0400

    Add files via upload

--- app/user.py
+++ app/user.py
@@ -1,1 +1,9 @@
-2
+from app import login_manager
+from flask_login import UserMixin
+
+fake_db = {'admin': {'password': 'p@$$w0rd'}}
+
+@login_manager.user_loader
+def load_user(email: str):  # could also be an asynchronous function
+    user = fake_db.get(email)
+    return user

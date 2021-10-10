c93c7823f26357491c46718e9a37f36f59250c4f
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-10 14:57:34 -0400

    Add files via upload

--- app/common_db.py
+++ app/common_db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import config as CONFIG
+import logging
 
 def is_present(table):
     conn = sqlite3.connect(CONFIG.DATABASE)
@@ -8,9 +9,12 @@ def is_present(table):
         c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='''+table+''' AND name='users' ''')
         if c.fetchone()[0]==1 :   
             found = True
+                logging.info("x1")
         else: 
             found = False  
+                logging.info("x2")
     except:
         found = False
+        logging.info("x3")
   
     return (found)

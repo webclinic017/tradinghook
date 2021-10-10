70bc907ca982269a78bf620053d13ff85ba384cc
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-10 14:58:37 -0400

    Add files via upload

--- app/common_db.py
+++ app/common_db.py
@@ -9,10 +9,10 @@ def is_present(table):
         c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='''+table+''' AND name='users' ''')
         if c.fetchone()[0]==1 :   
             found = True
-                logging.info("x1")
+            logging.info("x1")
         else: 
             found = False  
-                logging.info("x2")
+            logging.info("x2")
     except:
         found = False
         logging.info("x3")

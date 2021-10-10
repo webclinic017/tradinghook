f2a315cfd07f25cb2d247d97d51a30c9f540262b
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-10 15:58:49 -0400

    Update common_db.py

--- app/common_db.py
+++ app/common_db.py
@@ -6,17 +6,13 @@ def is_present(table):
     conn = sqlite3.connect(CONFIG.DATABASE)
     c = conn.cursor()
     try:
-        query = ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='''+table+''';'''
+        query = '''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{}';'''.format(table)
         print(query)
         c.execute(query)
         if c.fetchone()[0]==1 :   
             found = True
-            logging.info("x1")
         else: 
             found = False  
-            logging.info("x2")
     except:
         found = False
-        logging.info("x3")
-  
     return (found)

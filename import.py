6e003dc9ef9415388cdfb736c6bc20275fa5ebe7
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-17 22:30:36 -0400

    Add files via upload

--- import.py
+++ import.py
@@ -2,7 +2,7 @@ import json
 import os.path
 from app import tradecode
 
-if exits("exchangeaccounts.json")
+if exits("exchangeaccounts.json"):
     f = open('exchangeaccounts.json',)
  
     data = json.load(f)

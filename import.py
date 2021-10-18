b1f1c6592244d226477071e12c234bf20aa60769
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-10-17 22:28:50 -0400

    Add files via upload

--- import.py
+++ import.py
@@ -1,11 +1,22 @@
 import json
+import os.path
 from app import tradecode
 
-f = open('exchangeaccounts.json',)
+if exits("exchangeaccounts.json")
+    f = open('exchangeaccounts.json',)
  
-data = json.load(f)
+    data = json.load(f)
  
-for i in data['exchangeaccounts']:
-    print(i)
+    for i in data['exchangeaccounts']:
+        ea = ExchangeAccount()
+        eat.InstanceID = i["InstanceID"]
+        ea.UserID = i["UserID"]
+        ea.ExchangeID = i["ExchangeID"]
+        ea.exchangenickname = i["exchangenickname"]
+        ea.exchangeusername = i["exchangeusername"]
+        ea.exchangepassword = i["exchangepassword"]
+        ea.exchangetoken = i["exchangetoken"]
+        tradecode.insert_into_ExchangeAccount(ea)
+        print(i)
  
-f.close()
+    f.close()
